c7bf07aaf38cea49c52f7ff68733d778668b8554
Unknownflow <clarencelau88@gmail.com>
2026-09-17 00:44:42 +0800

    Fix checkstyle issues

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,9 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Remark} containing the given text.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
